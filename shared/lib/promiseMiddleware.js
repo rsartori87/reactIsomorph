9ea5f19db6f5b93c486f6ce3d03fd500149cac83
Riccardo Sartori <riccardo.sartori87@gmail.com>
2016-07-28 10:28:35 +0200

    aggiunti commenti a promiseMiddleware

--- shared/lib/promiseMiddleware.js
+++ shared/lib/promiseMiddleware.js
@@ -1,17 +1,21 @@
 export default function promiseMiddleware() {
     return next => action => {
+      // Scompongo l'azione in tipo, promessa e resto
         const { promise, type, ...rest } = action;
-
+      // Se l'azione non ha una chiave promessa continuo senza modifiche
         if (!promise) return next(action);
 
         const SUCCESS = type;
         const REQUEST = type + '_REQUEST';
         const FAILURE = type + '_FAILURE';
-
+      // Altrimenti intercetto l'azione ed informo lo store che è in corso
+      // una richiesta
         next({...rest, type: REQUEST});
 
         return promise
             .then(res => {
+              // Quando la promessa è soddisfatta ritorno l'azione originale
+              // aggiungendo la risposta alla chiave res
                 next({...rest, res, type: SUCCESS});
 
                 return true;
